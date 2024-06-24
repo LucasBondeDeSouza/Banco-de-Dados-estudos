47107a8ecf32160b7c50ad73ddf96fd8cbf17437
LucasBondeDeSouza <bondelucas77@gmail.com>
2024-06-24 14:02:07 -0300

    atualização

--- 04-Relacionamentos em Banco de Dados/Family Travel Tracker/queries.sql
+++ 04-Relacionamentos em Banco de Dados/Family Travel Tracker/queries.sql
@@ -102,12 +102,6 @@ name VARCHAR(15) UNIQUE NOT NULL,
 color VARCHAR(15)
 );
 
-CREATE TABLE visited_countries(
-id SERIAL PRIMARY KEY,
-country_code CHAR(2) NOT NULL,
-user_id INTEGER REFERENCES users(id)
-);
-
 INSERT INTO users (name, color)
 VALUES ('Angela', 'teal'), ('Jack', 'powderblue');
 
